b5010c32c9b0598703ae2711263b6590086e606f
Paulo Sergio <pscosta2008@gmail.com>
2024-05-18 21:59:50 -0300

    feat: organizing correctly the folders

--- coin_flip/lib/main.dart
+++ coin_flip/lib/main.dart
@@ -1,48 +1,6 @@
+import 'package:coin_flip/mainApp.dart';
 import 'package:flutter/material.dart';
 
 main() {
-  runApp(CoinFlipApp());
-}
-
-class CoinFlipApp extends StatelessWidget {
-  // nao sofrem modificação dinamica (requisicao API, variáveis... etc etc)
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: true, // false to hidde debug flag
-      theme: ThemeData.light(),
-      home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  @override
-  State<HomePage> createState() {
-    // conseguem mudanças dinamicas
-    return HomePageState();
-  }
-}
-
-class HomePageState extends State<HomePage> {
-  int counter = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: GestureDetector(
-          child: TextButton(
-            onPressed: () {
-              setState(() {
-                counter++;
-              });
-            },
-            child: Text('Clique aqui! ${counter}'),
-          ),
-        ),
-      ),
-    );
-  }
+  runApp(MainApp());
 }
