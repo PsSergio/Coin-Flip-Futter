9e68f66f6beca37ed9975a40cacd821fbe1bece7
Paulo Sergio <pscosta2008@gmail.com>
2024-05-18 21:53:20 -0300

    feat: Creating two new classes to build the home page. First time testing a logic button in Flutter

--- coin_flip/lib/main.dart
+++ coin_flip/lib/main.dart
@@ -1,19 +1,46 @@
 import 'package:flutter/material.dart';
 
 main() {
-  runApp(MainPage());
+  runApp(CoinFlipApp());
 }
 
-class MainPage extends StatelessWidget {
+class CoinFlipApp extends StatelessWidget {
+  // nao sofrem modificação dinamica (requisicao API, variáveis... etc etc)
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: true, // false to hidde debug flag
       theme: ThemeData.light(),
-      home: Container(
-        child: Center(
-          child: Text('First Test'),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() {
+    // conseguem mudanças dinamicas
+    return HomePageState();
+  }
+}
+
+class HomePageState extends State<HomePage> {
+  int counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: GestureDetector(
+          child: TextButton(
+            onPressed: () {
+              setState(() {
+                counter++;
+              });
+            },
+            child: Text('Clique aqui! ${counter}'),
+          ),
         ),
       ),
     );
