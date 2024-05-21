a44421dda73d36f646368533a16bf29d999b5c51
Paulo Sergio <pscosta2008@gmail.com>
2024-05-20 22:31:50 -0300

    feat: adding class to a custom button to simplify the development. This button is styled.

--- coin_flip/lib/home_page.dart
+++ coin_flip/lib/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:coin_flip/default_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:coin_flip/single_player.dart';
@@ -16,43 +17,44 @@ class HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.red,
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Image(image: AssetImage('images/cara.png'),),
 
-      appBar: AppBar(backgroundColor: Colors.red,),
-
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            // Image(image: AssetImage('images/cara.png'),),
-
-            Text(
-              'Coin Flip',
-              style: TextStyle(fontSize: 40.0, fontFamily: "Cocogoose", fontWeight: FontWeight.w400),
+              Text(
+                'Coin Flip',
+                style: TextStyle(
+                    fontSize: 40.0,
+                    fontFamily: "Cocogoose",
+                    fontWeight: FontWeight.w400),
               ),
 
-            ElevatedButton( // func to navigate to other page
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder:  (context) => SinglePlayerPage())
-                );
-              },
-              child: Text('Single Player'),
-            ),
+              Container(
+                height: 200,
+              ),
 
-            ElevatedButton( // func to navigate to other page
-              onPressed: () {
-                // Navigator.push(
-                //   context,
-                //   MaterialPageRoute(builder:  (context) => SinglePlayerPage())
-                // );
-              },
-              child: Text('2 Players'),
-            )
-          ],
-        
-        ),
-      )
-    );
+              DefaultButtonForApp(
+                text: 'SinglePlayer',
+                // func: navigateForPage(SinglePlayerPage(), context),
+              ),
+
+              Container(
+                height: 20,
+              ),
+            ],
+          ),
+        ));
   }
 }
+
+// void navigateForPage(Page, context){
+//   Navigator.push(
+//     context,
+//     MaterialPageRoute(builder:  (context) => Page())
+//   );
+// }
